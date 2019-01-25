d8b031ec6a895f7c977c02500889b60b7108f698
Ran Gu <earlguwang@gmail.com>
2019-01-24 21:07:58 -0800

    Adding fb notifications

--- Paul.py
+++ Paul.py
@@ -5,6 +5,8 @@ import requests
 import bs4 as bs
 import sendgrid
 from sendgrid.helpers.mail import *
+from fbchat import Client
+from fbchat.models import *
 
 TERM = '2019-03'
 WEBSOC = 'https://www.reg.uci.edu/perl/WebSoc?'
@@ -17,10 +19,12 @@ from_email = Email("AntAlmanac@gmail.com")
 qa_email = Email(os.environ.get('QA_EMAIL'))
 
 db = pymongo.MongoClient(os.environ.get('MONGODB_URI')).get_default_database()
-q, names = {}, {}
+emails, fbs, names = {}, {}, {}
 for course in db.queue.find():
-    q[course['code']] = course['addrs']
-    names[course['code']] = course['name']
+    code = course['code']
+    emails[code] = course['emails']
+    fbs[code] = course['fbs']
+    names[code] = course['name']
 
 def fetch_statuses(targets):
     statuses = {code:None for code in targets} #is statuses even a word in english? | initialize status values
@@ -49,14 +53,8 @@ def fetch_statuses(targets):
 
     return statuses
 
-statuses = fetch_statuses(q.keys())
+statuses = fetch_statuses(emails.keys())
 print(statuses)
-double_check = fetch_statuses([code for code, status in statuses.items() if status != 'FULL'])
-print(double_check)
-for code, status in double_check.items():
-    if status != statuses[code]: ## If the two samples disagree, then we say it's FULL
-        statuses[code] = 'FULL'
-print(double_check)
 
 for code, status in statuses.items():
     course_url = WEBSOC + urllib.parse.urlencode([('YearTerm',TERM),('CourseCodes',code),('ShowFinals',0),('ShowComments',0),('CancelledCourses','Include')])
@@ -64,19 +62,31 @@ for code, status in statuses.items():
         continue #keep waiting
     else:
         if status == 'Waitl':
-            msg = '<html><p>Space just opened up <strong>on the waitlist</strong> for {} with code, {} ({}).</p>'.format(names[code], code, course_url)
+            msg = 'Space just opened up on the waitlist for {} with code, {} ({}).'.format(names[code], code, course_url)
         if status == 'OPEN':
-            msg = '<html><p>Space just opened up in {} with code, {} ({}).</p>'.format(names[code], code, course_url)
+            msg = 'Space just opened up in {} with code, {} ({}).'.format(names[code], code, course_url)
 
     ##send notifications
-    for house in q[code]:
+    print('emails')
+    for house in emails[code]:
         to_email = Email(house)
         subject = "[AntAlmanac Notifications] Space Just Opened Up to Enroll"
-        content = Content("text/html", msg+'<p>Here\'s WebReg while we\'re at it: <a href="https://www.reg.uci.edu/registrar/soc/webreg.html" target="_blank">WebReg</a></p><p>You have been removed from this watchlist; to add yourself again, please visit <a href="https://antalmanac.com" target="_blank">AntAlmanac</a> or click on <a href="http://mediaont.herokuapp.com/{}/{}/{}" target="_blank">this link</a></p><p>Also, was this notification correct? Were you able to add yourself? Please do let us know asap if there is anything that isn\'t working as it should be!!! <a href="https://goo.gl/forms/U8CuPs05DlIbrSfz2" target="_blank">Give (anonymous) feedback!</a></p><p>Yours sincerely,</p><p>Poor Peter\'s AntAlmanac</p></html>'.format(code, names[code], house))
+        content = Content("text/html",'<html><p>'+msg+'</p><p>Here\'s WebReg while we\'re at it: <a href="https://www.reg.uci.edu/registrar/soc/webreg.html" target="_blank">WebReg</a></p><p>You have been removed from this watchlist; to add yourself again, please visit <a href="https://antalmanac.com" target="_blank">AntAlmanac</a> or click on <a href="http://mediaont.herokuapp.com/{}/{}/{}" target="_blank">this link</a></p><p>Also, was this notification correct? Were you able to add yourself? Please do let us know asap if there is anything that isn\'t working as it should be!!! <a href="https://goo.gl/forms/U8CuPs05DlIbrSfz2" target="_blank">Give (anonymous) feedback!</a></p><p>Yours sincerely,</p><p>Poor Peter\'s AntAlmanac</p></html>'.format(code, names[code], house))
         mail = Mail(from_email, subject, to_email, content)
         response = sg.client.mail.send.post(request_body=mail.get())
         mail = Mail(from_email, subject, qa_email, content) #For quality assurance purposes
         response = sg.client.mail.send.post(request_body=mail.get()) #For quality assurance
-        print(code, 'Done')
+        print(code)
+
+    print('fbs')
+    client = Client(os.environ.get('USERNAME'), os.environ.get('PASSWORD'))
+    for fb in fbs[code]:
+        client.send(Message(text='AntAlmanac Notifications!!'), thread_id=fb, thread_type=ThreadType.USER)
+        client.send(Message(text=msg), thread_id=fb, thread_type=ThreadType.USER)
+        client.send(Message(text='Here\'s WebReg while we\'re at it: https://www.reg.uci.edu/registrar/soc/webreg.html'), thread_id=fb, thread_type=ThreadType.USER)
+        client.send(Message(text='You have been removed from this watchlist; to add yourself again, please click on http://mediaont.herokuapp.com/{}/{}/{}'.format(code, names[code], house)), thread_id=fb, thread_type=ThreadType.USER)
+        client.send(Message(text='Also, was this notification correct? Were you able to add yourself? Please do let us know asap if there is anything that isn\'t working as it should be!!! https://goo.gl/forms/U8CuPs05DlIbrSfz2'), thread_id=fb, thread_type=ThreadType.USER)
+        print(code)
+    client.logout()
 
     db.queue.delete_one({"code": str(code)})
--- app.py
+++ app.py
@@ -1,18 +1,49 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
+from fbchat import Client
+from fbchat.models import *
 import pymongo
 import os
+import redis
 
 app = Flask(__name__)
 CORS(app)
 db = pymongo.MongoClient(os.environ.get('MONGODB_URI')).get_default_database()
+r = redis.from_url(os.environ.get('REDISCLOUD_URL'))
 
-@app.route("/<code>/<name>/<email>")
-def add(code, name, email):
+@app.route("/email/<code>/<name>/<email>")
+def add_email(code, name, email):
     doc = db["queue"].find_one({"code": code})
-    if doc is None:
-        db["queue"].insert_one({"code": code, "name":name, "addrs": [email]})
-    elif email not in doc["addrs"]:
-        doc["addrs"].append(email)
+    if doc is None: #course not in db yet
+        db["queue"].insert_one({"code": code, "name":name, "emails": [email], "fbs": []})
+    elif email not in doc["emails"]: #email not added already
+        doc["emails"].append(email)
         db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
-    return '<html><body><h1>{} has been added to the watchlist for {}!</h1></body></html>'.format(email,code)
+    else: #already in the db
+        return '<html><body><h1 id=\"findme\">{} is already on the email watchlist for {} {}!</h1></body></html>'.format(email,code,name)
+    return '<html><body><h1 id=\"findme\">{} has been added to the email watchlist for {} {}!</h1></body></html>'.format(email,code,name)
+
+@app.route("/facebook/<code>/<name>/<fb>")
+def add_fb(code, name, fb):
+    try:
+        watchlisted = eval(r.get(fb))
+        watchlisted.add(code)
+        r.set(fb,str(watchlisted))
+    except: #user's first time
+        client = Client(os.environ.get('USERNAME'), os.environ.get('PASSWORD'))
+        try:
+            client.send(Message(text='Hi! AntAlmanac here. This is to confirm that you just signed up for FB Messenger notifications. Please respond \"zot\" to this message or move this conversation so that you can get notified the next time we message. If you did not initiate this request, please ignore this message. Thank you!'),
+                    thread_id=fb, thread_type=ThreadType.USER)
+        except:
+            return '<html><body><h1 id=\"findme\">{} is an invalid Facebook id!</h1></body></html>'.format(fb)
+        r.set(fb,str({code}))
+        client.logout()
+    doc = db["queue"].find_one({"code": code})
+    if doc is None: #course not in db yet
+        db["queue"].insert_one({"code": code, "name":name, "emails":[], "fbs": [fb]})
+    elif fb not in doc["fbs"]: #if fb not added already
+        doc["fbs"].append(fb)
+        db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
+    else: #already in the db
+        return '<html><body><h1 id=\"findme\">{} is already on the FB watchlist for {} {}!</h1></body></html>'.format(fb,code,name)
+    return '<html><body><h1 id=\"findme\">{} has been added to the FB watchlist for {} {}!</h1></body></html>'.format(fb,code,name)
