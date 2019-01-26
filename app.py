4183ba271ac39814bd92319a9848d5f4bb5cccd2
pistachio05 <malaya@uci.edu>
2019-01-26 12:18:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -48,29 +48,3 @@ def add_fb(code, name, fb):
         return '<html><body><h1 id=\"findme\">{} is already on the FB watchlist for {} {}!</h1></body></html>'.format(fb,code,name)
     return '<html><body><h1 id=\"findme\">{} has been added to the FB watchlist for {} {}!</h1></body></html>'.format(fb,code,name)
 
-# template route for AntAlmanac endpoint:
-@app.route("/fbmessenger/<code>/<name>/<fb>")
-def add_fb(code, name, fb):
-    if r.get(fb) == None:
-        r.set(fb,'f')
-        client = Client(os.environ.get('USERNAME'), os.environ.get('PASSWORD'))
-        try:
-            client.send(Message(text='Hi! AntAlmanac here. This is to confirm that you just signed up for FB Messenger notifications. Please respond \"zot\" to this message or move this conversation so that you can get notified the next time we message. If you did not initiate this request, please ignore this message. Thank you!'),
-                    thread_id=fb, thread_type=ThreadType.USER)
-        except:
-            payload = {"code":-1,"message":"{} is an invalid Facebook id!".format(fb)}
-            return json.dumps(payload)
-
-        client.logout()
-
-    doc = db["queue"].find_one({"code": code})
-    if doc is None: #course not in db yet
-        db["queue"].insert_one({"code": code, "name":name, "emails":[], "fbs": [fb]})
-    elif fb not in doc["fbs"]: #if fb not added already
-        doc["fbs"].append(fb)
-        db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
-    else: #already in the db
-    	payload = {"code":1,message":"{} "is already on the FB watchlist for {} {}".format(fb,code,name)}
-        return json.dumps(payload)
-    payload = {"message":"{} has been added to the FB watchlist for {} {}".format(fb,code,name)}
-    return json.dumps(payload)
