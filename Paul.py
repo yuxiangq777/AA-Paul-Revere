f32b17f77d493beb4c7c1518325531cc647f2de2
Ran Gu <earlguwang@gmail.com>
2019-02-19 15:20:53 -0800

    Now with SMS

--- Paul.py
+++ Paul.py
@@ -1,9 +1,10 @@
 import pymongo
-import os
 import urllib.parse
 import requests
 import bs4 as bs
 import sendgrid
+import config
+from twilio.rest import Client
 from sendgrid.helpers.mail import *
 
 TERM = '2019-03'
@@ -12,16 +13,23 @@ BATCH_SIZE = 8
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+# initialize senders
+twilio = Client(config.ACCOUNT_SID, config.AUTH_TOKEN)
+sg = sendgrid.SendGridAPIClient(config.SENDGRID_API_KEY)
 from_email = Email("AntAlmanac@gmail.com")
-qa_email = Email(os.environ.get('QA_EMAIL'))
+qa_email = Email(config.QA_EMAIL)
 
-db = pymongo.MongoClient(os.environ.get('MONGODB_URI')).get_default_database()
-q, names = {}, {}
+db = pymongo.MongoClient(config.MONGODB_URI).get_default_database()
+
+## initialize variables...
+emails, nums, names = {}, {}, {}
 for course in db.queue.find():
-    q[course['code']] = course['addrs']
-    names[course['code']] = course['name']
+    code = course['code']
+    emails[code] = course['emails']
+    nums[code] = course['nums']
+    names[code] = course['name']
 
+##helpers
 def fetch_statuses(targets):
     statuses = {code:None for code in targets} #is statuses even a word in english? | initialize status values
 
@@ -49,14 +57,8 @@ def fetch_statuses(targets):
 
     return statuses
 
-statuses = fetch_statuses(q.keys())
+statuses = fetch_statuses(emails.keys())
 print(statuses)
-double_check = fetch_statuses([code for code, status in statuses.items() if status != 'FULL'])
-print(double_check)
-for code, status in double_check.items():
-    if status != statuses[code]: ## If the two samples disagree, then we say it's FULL
-        statuses[code] = 'FULL'
-print(double_check)
 
 for code, status in statuses.items():
     course_url = WEBSOC + urllib.parse.urlencode([('YearTerm',TERM),('CourseCodes',code),('ShowFinals',0),('ShowComments',0),('CancelledCourses','Include')])
@@ -64,19 +66,31 @@ for code, status in statuses.items():
         continue #keep waiting
     else:
         if status == 'Waitl':
-            msg = '<html><p>Space just opened up <strong>on the waitlist</strong> for {} with code, {} ({}).</p>'.format(names[code], code, course_url)
+            msg = 'Space opened up on the waitlist for {} with code, {} ({}). '.format(names[code], code, course_url)
         if status == 'OPEN':
-            msg = '<html><p>Space just opened up in {} with code, {} ({}).</p>'.format(names[code], code, course_url)
+            msg = 'Space opened up in {} with code, {} ({}). '.format(names[code], code, course_url)
 
-    ##send notifications
-    for house in q[code]:
+    ##send email notifications
+    print('emails')
+    for house in emails[code]:
         to_email = Email(house)
         subject = "[AntAlmanac Notifications] Space Just Opened Up to Enroll"
-        content = Content("text/html", msg+'<p>Here\'s WebReg while we\'re at it: <a href="https://www.reg.uci.edu/registrar/soc/webreg.html" target="_blank">WebReg</a></p><p>You have been removed from this watchlist; to add yourself again, please visit <a href="https://antalmanac.com" target="_blank">AntAlmanac</a> or click on <a href="http://mediaont.herokuapp.com/{}/{}/{}" target="_blank">this link</a></p><p>Also, was this notification correct? Were you able to add yourself? Please do let us know asap if there is anything that isn\'t working as it should be!!! <a href="https://goo.gl/forms/U8CuPs05DlIbrSfz2" target="_blank">Give (anonymous) feedback!</a></p><p>Yours sincerely,</p><p>Poor Peter\'s AntAlmanac</p></html>'.format(code, names[code], house))
+        content = Content("text/html",'<html><p>'+msg+'</p><p>Here\'s WebReg while we\'re at it: <a href="https://www.reg.uci.edu/registrar/soc/webreg.html" target="_blank">WebReg</a></p><p>You have been removed from this watchlist; to add yourself again, please visit <a href="https://antalmanac.com" target="_blank">AntAlmanac</a> or click on <a href="{}/email/{}/{}/{}" target="_blank">this link</a></p><p>Also, was this notification correct? Were you able to add yourself? Please do let us know asap if there is anything that isn\'t working as it should be!!! <a href="https://goo.gl/forms/U8CuPs05DlIbrSfz2" target="_blank">Give (anonymous) feedback!</a></p><p>Yours sincerely,</p><p>Poor Peter\'s AntAlmanac</p></html>'.format(config.BASE_URL, code, names[code], house))
         mail = Mail(from_email, subject, to_email, content)
         response = sg.client.mail.send.post(request_body=mail.get())
         mail = Mail(from_email, subject, qa_email, content) #For quality assurance purposes
         response = sg.client.mail.send.post(request_body=mail.get()) #For quality assurance
-        print(code, 'Done')
+        print(code)
+
+    ##send sms notifications
+    print('sms')
+    if client != None:
+        for num in nums[code]:
+            sms_msg = 'AntAlmanac Notifications: ' + msg + 'WebReg(https://www.reg.uci.edu/registrar/soc/webreg.html).' + 'You have been removed from this watchlist; to add yourself again: {}/sms/{}/{}/{}'.format(config.BASE_URL, code, names[code], num))
+            message = client.messages.create(from_=config.FROM_NUMBER, body=sms_msg,to='+1'+num)
+            print(code)
 
     db.queue.delete_one({"code": str(code)})
+
+if client != None:
+    client.logout()
--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
 import pymongo
-import os
+import config
 
 app = Flask(__name__)
 CORS(app)
-db = pymongo.MongoClient(os.environ.get('MONGODB_URI')).get_default_database()
+db = pymongo.MongoClient(config.MONGODB_URI).get_default_database()
+r = redis.from_url(config.REDISCLOUD_URL)
 
-@app.route("/<code>/<name>/<email>")
-def add(code, name, email):
+@app.route("/email/<code>/<name>/<email>")
+def add_email(code, name, email):
+    if r.get(email) == None: #first time
+        r.set(email,'e')
     doc = db["queue"].find_one({"code": code})
-    if doc is None:
-        db["queue"].insert_one({"code": code, "name":name, "addrs": [email]})
-    elif email not in doc["addrs"]:
-        doc["addrs"].append(email)
+    if doc is None: #course not in db yet
+        db["queue"].insert_one({"code": code, "name":name, "emails": [email], "nums": []})
+    elif email not in doc["emails"]: #email not added already
+        doc["emails"].append(email)
         db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
-    return '<html><body><h1>{} has been added to the watchlist for {}!</h1></body></html>'.format(email,code)
+    else: #already in the db
+        return '<html><body><h1 id=\"findme\">{} is already on the email watchlist for {} {}!</h1></body></html>'.format(email,code,name)
+    return '<html><body><h1 id=\"findme\">{} has been added to the email watchlist for {} {}!</h1></body></html>'.format(email,code,name)
+
+@app.route("/sms/<code>/<name>/<num>")
+def add_sms(code, name, num):
+    if r.get(num) == None: #first time
+        r.set(num,'s')
+    doc = db["queue"].find_one({"code": code})
+    if doc is None: #course not in db yet
+        db["queue"].insert_one({"code": code, "name":name, "emails": [], "nums": [num]})
+    elif num not in doc["nums"]: #number not added already
+        doc["nums"].append(num)
+        db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
+    else: #already in the db
+        return '<html><body><h1 id=\"findme\">{} is already on the sms watchlist for {} {}!</h1></body></html>'.format(num,code,name)
+    return '<html><body><h1 id=\"findme\">{} has been added to the sms watchlist for {} {}!</h1></body></html>'.format(num,code,name)
