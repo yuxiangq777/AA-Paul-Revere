924de455ada41cd9cb9e6057d1155da60012e558
yuxiangq777 <44986516+yuxiangq777@users.noreply.github.com>
2019-03-29 23:10:31 -0700

    fixed the tinyurl

--- Paul.py
+++ Paul.py
@@ -10,7 +10,7 @@ import boto3
 
 TERM = '2019-14'
 WEBSOC = 'https://www.reg.uci.edu/perl/WebSoc?'
-
+TINYURL="http://tinyurl.com/api-create.php"
 BATCH_SIZE = 8
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
