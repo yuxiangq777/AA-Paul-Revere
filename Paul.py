49b7fb59dc2b0a605432dd32023e6f1f270cf210
justinekoa <justinekoa@gmail.com>
2019-03-30 00:39:47 -0700

    url shortening

--- Paul.py
+++ Paul.py
@@ -19,6 +19,20 @@ aws = boto3.client(
     aws_secret_access_key=config.AWS_SECRECTKEY,
     region_name="us-east-1"
 )
+
+TINY_URL = "http://tinyurl.com/api-create.php"
+
+def shorten(long_url):
+    try:
+        url = TINY_URL + "?" \
+            + urllib.parse.urlencode({"url": long_url})
+        res = requests.get(url)
+        # print("LONG URL:", long_url) # original url
+        # print("SHORT URL:", res.text) # shortened version
+        return res.text # return shorten url
+    except Exception as e:
+        return long_url # if unable to get shorten url, just return back the long one
+
 # initialize senders
 while 1:
     print("run")
@@ -28,7 +42,7 @@ while 1:
 
     db = pymongo.MongoClient(config.MONGODB_URI).get_database()
 
-    ## initialize variables...
+    # initialize variables...
     emails, nums, names = {}, {}, {}
     for course in db.queue.find():
         code = course['code']
@@ -52,7 +66,7 @@ while 1:
             # get status values for these codes
             fields = [('YearTerm',TERM),('CourseCodes',', '.join(codes)),('ShowFinals',0),('ShowComments',0),('CancelledCourses','Include')]
             url = WEBSOC + urllib.parse.urlencode(fields)
-            #print(url)
+            # print(url)
 
             sp = bs.BeautifulSoup(requests.get(url, headers=HEADERS).content, 'lxml')
 
@@ -65,22 +79,22 @@ while 1:
         return statuses
 
     statuses = fetch_statuses(emails.keys())
-#    print(statuses)
+   # print(statuses)
 
     for code, status in statuses.items():
         if status is None:
             course_url = WEBSOC + urllib.parse.urlencode([('YearTerm',TERM),('CourseCodes',code),('CancelledCourses','Include')])
-            msg = '{}. Code: {} ({}) has been cancelled'.format(names[code], code, course_url)
+            msg = '{}. Code: {} ({}) has been cancelled'.format(names[code], code, shorten(course_url)) # shorten the course_url
         elif status == 'FULL' or status == 'NewOnly':
             continue #keep waiting
         else:
-            course_url = WEBSOC + urllib.parse.urlencode([('YearTerm',TERM),('CourseCodes',code)])
+            course_url = shorten(WEBSOC + urllib.parse.urlencode([('YearTerm',TERM),('CourseCodes',code)])) # shorten the course_url
             if status == 'Waitl':
                 msg = 'Space opened on waitlist for {}. Code: {} ({}). '.format(names[code], code, course_url)
             if status == 'OPEN':
                 msg = 'Space opened in {}. Code: {} ({}). '.format(names[code], code, course_url)
 
-        ##send email notifications
+        #send email notifications
         print('emails')
         for house in emails[code]:
             to_email = Email(house)
@@ -91,14 +105,17 @@ while 1:
             content = Content("text/html",'<html><p>'+msg+'</p><p>Here\'s WebReg while we\'re at it: <a href="https://www.reg.uci.edu/registrar/soc/webreg.html" target="_blank">WebReg</a></p><p>You have been removed from this watchlist; to add yourself again, please visit <a href="https://antalmanac.com" target="_blank">AntAlmanac</a> or click on {}/email/{}/{}/{}</p><p>Also, was this notification correct? Were you able to add yourself? Please do let us know asap if there is anything that isn\'t working as it should be!!! <a href="https://goo.gl/forms/U8CuPs05DlIbrSfz2" target="_blank">Give (anonymous) feedback!</a></p><p>Yours sincerely,</p><p>Poor Peter\'s AntAlmanac</p></html>'.format(config.BASE_URL, code, urllib.parse.quote(names[code]), house))
             mail = Mail(from_email, subject, qa_email, content) #For quality assurance purposes
             response = sg.client.mail.send.post(request_body=mail.get()) #For quality assurance
-            #print(code)
+            print(code)
 
-        ##send sms notifications
+        #send sms notifications
         print('sms')
+
         if aws != None:
             for num in nums[code]:
                 try:
-                    sms_msg = 'AntAlmanac: ' + msg + 'To add back to watchlist: {}/sms/{}/{}/{}'.format(config.BASE_URL, code, urllib.parse.quote(names[code]), num)
+                    long_url = '{}/sms/{}/{}/{}'.format(config.BASE_URL, code, urllib.parse.quote(names[code]), num)
+                    sms_msg = 'AntAlmanac: ' + msg + 'To add back to watchlist: ' + shorten(long_url)
+                    print(sms_msg)
                     aws.publish(
                         PhoneNumber="+1"+num,
                         Message=sms_msg
